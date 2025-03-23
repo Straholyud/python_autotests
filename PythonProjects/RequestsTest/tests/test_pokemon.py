9d9654e66954b6a28afbda5cc44f9647204f625d
Straholyud <wolk.maksim@yandex.ru>
2025-03-23 08:45:37 +0300

    test_pokemon.py

--- PythonProjects/RequestsTest/tests/test_pokemon.py
+++ PythonProjects/RequestsTest/tests/test_pokemon.py
@@ -7,9 +7,15 @@ HEADER = {'Content-Type':'application/json', 'trainer_token':TOKEN}
 TRAINER_ID = '24867'
 
 def test_status_200():
-    responce = requests.get(url=f'{URL}/pokemons', params={'trainer_id':TRAINER_ID})
-    assert responce.status_code== 200
+    response = requests.get(url=f'{URL}/trainers', params={'trainer_id':TRAINER_ID})
+    assert response.status_code== 200
 
 def test_name_trainer():
-    response_get = requests.get(url=f'{URL}/pokemons', params={'trainer_id':TRAINER_ID})
-    assert response.json()['data'][0]['trainer_name'] == 'Flin'
+    response_get = requests.get(url=f'{URL}/trainers', params={'trainer_id':TRAINER_ID})
+    assert response_get.json()["data"][0]["trainer_name"] == 'Flin'
+    print(response_get.text)
+
+@pytest.mark.parametrize('key, value',[('name', 'Flin'), ('trainer_id', TRAINER_ID)], ('trainer_id', '24867'))
+def test_parametrize(key,value):
+    responce_parametrize = requests.get(url=f'{URL}/trainers', params={'trainer_id':TRAINER_ID})
+    assert responce_parametrize.json()["data"][0][key] == value
